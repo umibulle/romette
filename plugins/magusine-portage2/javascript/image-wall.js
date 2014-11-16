b81a71889a8aca81026d10f7847023183bbf1f2c
Umi Bulle <elise@umi-bulle.fr>
2014-11-16 22:09:41 +0100

    Création page spectacle

---
+++ plugins/magusine-portage2/javascript/image-wall.js
@@ -0,0 +1,10 @@
+/*
+ * Image Wall
+ * (c) Umi 2014
+ */
+/*var container = document.querySelector('#image-wall-ul');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 20%,
+  itemSelector: 'li'
+});*/
